2c4ab51e435044bf42b3bc2eab5b2f7e0de08c07
lucagez <lucagesmundo@yahoo.it>
2020-07-05 10:32:30 +0200

    add cache-utils docs

--- packages/client/src/cache-utils.js
+++ packages/client/src/cache-utils.js
@@ -2,6 +2,11 @@ import { modern } from './features';
 
 const DEFAULT_CACHE_NAME = '__REMOTE_COMPONENTS__v1';
 
+/**
+ * clearCache.
+ * 
+ * Destroy current cache
+ */
 const _clearCache = async () => {
   const keys = await caches.keys();
   const current = keys.find(key => key === DEFAULT_CACHE_NAME);
@@ -9,6 +14,13 @@ const _clearCache = async () => {
   caches.delete(current);
 };
 
+/**
+ * clearEntries.
+ * 
+ * Remove entries from cache
+ * 
+ * @param {array} entries - Url to be removed from cache
+ */
 const _clearEntries = (entries) => {
   const current = caches.open(DEFAULT_CACHE_NAME);
   const deletes = entries.map(entry => current.delete(entry));
