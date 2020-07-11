b8ce2c334a9c048f89bab514143d950f0230906c
lucagez <lucagesmundo@yahoo.it>
2020-07-11 12:04:29 +0200

    move contextify to remotemport

--- packages/client/src/import.js
+++ packages/client/src/import.js
@@ -5,20 +5,28 @@ import { contextify } from './contextify';
 
 const remoteImport = async ({
   url,
-  cacheStrategy = 'none'
+  dependencies = {},
+  cacheStrategy = 'none',
 }) => {
-  const fetchSource = modern && cacheStrategy !== 'none'
-    ? swrImport
-    : legacyImport;
+  const fetchSource =
+    modern && cacheStrategy !== 'none' ? swrImport : legacyImport;
 
   if (hasComponent(url)) {
     return getComponent(url);
   }
 
-  const source = await fetchSource(url, {
-    onError: console.error,
-    onDone: () => {
-      
-    }
-  });
+  try {
+    const source = await fetchSource(url);
+
+    /**
+     * Evaluating source in a mocked context.
+     * Providing ad-hoc module, exports and require objects.
+     * Dependencies inside evaluated module are scoped.
+     * This is making possible to host multiple versions
+     *  of conflicting dependencies in the same page.
+     */
+    contextify(url, source, dependencies);
+
+
+  } catch (error) {}
 };
