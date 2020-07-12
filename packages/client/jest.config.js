a9eeeeada5650821fedc299273ac51e7569aa1c4
lucagez <lucagesmundo@yahoo.it>
2020-07-12 08:36:03 +0200

    update jest config

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
   rootDir: path.resolve(__dirname, './tests'),
-  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  testMatch: ['**/?(*.)+(test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
   preset: 'jest-puppeteer',
   testEnvironment: 'jsdom',
